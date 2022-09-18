86b04291e0646880c06b28ef6c7459790daf4401
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2022-09-18 15:38:23 -0300

    fix: add bootstrap script and sleep function to wait until finished

--- infrastructure/efs.tf
+++ infrastructure/efs.tf
@@ -1,4 +1,3 @@
-# Creating EFS file system
 resource "aws_efs_file_system" "efs" {
   creation_token = "${local.prefix}-efs"
   tags = {
@@ -6,16 +5,14 @@ resource "aws_efs_file_system" "efs" {
   }
 }
 
-# Creating Mount target of EFS
 resource "aws_efs_mount_target" "mount" {
   file_system_id  = aws_efs_file_system.efs.id
   subnet_id       = aws_subnet.this.id
   security_groups = [aws_security_group.sg-web.id]
 }
 
-# Creating Mount Point for EFS
 resource "null_resource" "configure_nfs" {
-  depends_on = [aws_efs_mount_target.mount]
+  depends_on = [aws_efs_mount_target.mount, aws_instance.this]
   connection {
     type        = "ssh"
     user        = "ubuntu"
@@ -24,36 +21,14 @@ resource "null_resource" "configure_nfs" {
   }
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install nfs-common -y",
-      # "sudo apt-get install python3.8 -y",
-      # "sudo apt-get install python3-pip -y",
-      # "python --version",
-      # "python3 --version",
-      # "echo ${aws_efs_file_system.efs.dns_name}",
-      # # "ls -la",
-      # # "pwd",
-      # "sudo mkdir -p /usr/share/nginx/html",
-      # "ls -la",
-      "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.efs.dns_name}:/ /usr/share/nginx/html",
-      "sudo cd /usr/share/nginx/html",
+      "sleep 90", # wait for the user_data.sh to finish
+      "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.efs.dns_name}:/ /efs",
+      "cd /efs",
+      "sudo rm -rf aws-website-with-iac",
       "sudo git clone https://github.com/MatheusKraisfeld/aws-website-with-iac.git",
-      "sudo cd aws-website-with-iac",
+      "cd aws-website-with-iac",
       "sudo docker build . -t website:latest",
       "sudo docker run -d -p 80:80 website:latest",
-      # "ls",
-      # "sudo chown -R ubuntu.ubuntu mount-point",
-      # "cd mount-point",
-      # "ls",
-      # "mkdir access",
-      # "sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1",
-      # "sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2",
-      # "printf '2\n' | sudo update-alternatives --config python3",
-      # "pwd",
-      # "ls -la",
-      # "echo 'Python version:'",
-      # "python3 --version",
-      # "pip3 install --upgrade --target ./access/ numpy --system"
     ]
   }
 }
